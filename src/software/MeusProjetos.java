ad5dc5d5602874ae6e51f6dd9bbb9b821da0b9b1
rhebecaabreu <rhebecaabreu@gmail.com>
2017-06-18 01:03:41 -0400

    nova classe, meus projetos

---
+++ src/software/MeusProjetos.java
@@ -0,0 +1,45 @@
+package software;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MeusProjetos extends JFrame {
+
+	private JPanel contentPane;
+	private Projeto nprojeto;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MeusProjetos frame = new MeusProjetos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MeusProjetos() {
+		nprojeto=new Projeto();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
