519386ebbd1e7446d5121e444ff48211fd3be93e
rhebecaabreu <rhebecaabreu@gmail.com>
2017-08-15 14:15:58 -0400

    [Tabela] Interessado
    Criação da tabela para interessados.

--- database/SoftwareIC-Script-base.sql
+++ database/SoftwareIC-Script-base.sql
@@ -23,6 +23,18 @@ create table if not exists requisito(
 	tipoRequisito enum('FUNCIONAL', 'NAOFUNCIONAL', 'USUARIO', 'NEGOCIO') not null
 );
 
+create table if not exists interessado(
+	codigo integer auto_increment not null primary key,
+	nome varchar(100),
+    sobrenome varchar(100),
+    endereco varchar(300),
+    rg varchar(14),
+    cpf varchar(14),
+    telefone varchar(15),
+    email varchar(50)
+
+);
+
 alter table requisito add column pcodigo int not null;
 alter table requisito add foreign key (pcodigo) references projeto(codigo)
 			on delete restrict 
